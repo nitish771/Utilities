f00382ea6fc614843d4e7ec90b2d7f274a04bf20
kcuf <kcuf@localhost.localdomain>
2022-04-06 22:34:41 +0530

    Imporve zip function - Add password - recursive/single directory compression - default name and location

--- compress.py
+++ compress.py
@@ -5,19 +5,40 @@ import os
 
 
 
-def zip(dir_, save_as, pwd=None):
+def zip(dir_, save_as=None, comp_sub_dir=False, pwd=None):
 	'''Zip a directory
 	Params
 		dir_: base_dir to zip
 		save_as: save name of folder. use absolute dir to save to another location
 	'''
-	if pwd is not None:
-		folder = os.path.dirname(dir_) # .split('/')[-1]
-		print(folder)
-		os.chdir(folder)
-		folder = os.path.basename(dir_) # .split('/')[-1]
-		cmd = f'zip -P {pwd} -r {save_as}.zip {folder}'
-		os.system(cmd)
+	cmd = 'zip '
+	folder = dir_
+	file_name = os.path.basename(dir_)
+
+	if save_as is None:
+		save_as = os.path.dirname(dir_)
+
+	if not pwd and not comp_sub_dir :
+		shutil.make_archive(dir_, 'zip', dir_)
 		return
 
-	shutil.make_archive(save_as, 'zip', home_dir)
+	if pwd is not None:
+		cmd += '-P ' + pwd
+
+
+	if comp_sub_dir is True:  # compress directory with sub-directories
+		os.chdir(folder)
+		if not os.path.exists(save_as):
+			os.mkdir(save_as)
+		for fold in os.listdir():
+			new_fold = folder + '/' + fold
+			if os.path.isdir(new_fold):
+				sub_cmd = cmd + f' -r "{save_as}/{fold}.zip" "{fold}"'
+				os.system(sub_cmd)
+				print(sub_cmd)
+				sub_cmd = cmd 
+	else:  # just single directory
+		folder = os.path.dirname(dir_)
+		os.chdir(folder)
+		cmd += f' -r "{save_as}.zip" "{file_name}"'
+		os.system(cmd)
